ddb6462192ae355613650a86af9c7a0bf4e6fcee
mizchi <miz404@gmail.com>
2025-10-26 00:27:24 +0900

    react

--- run.ts
+++ run.ts
@@ -1,32 +1,32 @@
 // import { h, render } from "preact";
 // import { useState } from "preact/hooks";
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useMemo,
-  useRef,
-} from "react";
+import React from "react";
+import * as ReactApi from "react";
+
 import { createRoot } from "react-dom/client";
 console.log("[js]Starting app...");
 
 // @ts-ignore xxx
-globalThis.ReactLib = {
-  createElement: React.createElement,
-  useState: useState,
-  useMemo: useMemo,
-  useEffect: useEffect,
-  useRef: useRef,
-  useCallback: useCallback,
+globalThis.__ReactApi = {
+  ...ReactApi,
+  // createElement: React.createElement,
+  // useState: useState,
+  // useMemo: useMemo,
+  // useEffect: useEffect,
+  // useRef: useRef,
+  // createContext: createContext,
+  // useContext: useContext,
+  // useCallback: useCallback,
+  // Fragment: Fragment,
   render(element: React.ReactElement, container: any) {
     const root = createRoot(container);
     root.render(element);
   },
 };
 
-async function js_main() {
+async function main() {
   // @ts-ignore xxx
   await import("./target/js/release/build/examples/examples.js");
 }
 
-await js_main();
+await main();
